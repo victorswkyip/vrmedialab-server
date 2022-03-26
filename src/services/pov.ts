0ad890354e4d666966156429cdb085a2075450fe
Victor Yip <victor@Victors-MacBook-Pro.local>
2022-03-25 21:56:16 -0400

    updated pov and added .gitignore

--- src/services/pov.ts
+++ src/services/pov.ts
@@ -1,8 +1,15 @@
 import { Quaternion, Vector3 } from "three";
 
+//@param id: wss identifier
+//@param name: lens identifier
+//@@param position: position in game
+//@param rotation: rotation in game
+//@param audio id: spatial audio identifier
 export interface Pov {
     id: string;
     name?: string;
     position?: Vector3;
     rotation?: Quaternion;
+    audio_id?: string;
+    
   }
